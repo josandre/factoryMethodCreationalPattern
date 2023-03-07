8b9bc25feb5164208961c54bcc3c7a5c427c28a4
Jocselyn Aguilar <ajocseline@gmail.com>
2023-03-06 20:57:59 -0600

    loading json file

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,7 +8,10 @@ module com.example.factorymethod {
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
     requires com.almasb.fxgl.all;
+    requires com.fasterxml.jackson.databind;
 
     opens com.example.factorymethod to javafx.fxml;
     exports com.example.factorymethod;
+    exports com.example.factorymethod.View;
+
 }
